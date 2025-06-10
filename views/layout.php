2b57744f9d4ad379d91de0530096170a3f129aa7
jmp035 <jacobmoratayapedroza@gmail.com>
2025-06-10 09:00:58 -0600

    Añadir enlace de navegación para "Marcas" y ajustar formato del enlace "clientes" en el menú de navegación

--- views/layout.php
+++ views/layout.php
@@ -26,7 +26,10 @@
 
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin: 0;">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/app03_jmp/clientes"><i class="bi bi-person-check-fill"></i></i>clientes</a>
+                        <a class="nav-link" aria-current="page" href="/app03_jmp/clientes"> <i class="bi bi-person-check-fill"></i> </i>clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/app03_jmp/marcas"> <i class="bi bi-r-circle"></i></i> </i>Marcas</a>
                     </li>
 
 
@@ -39,9 +42,6 @@
                             <li>
                                 <a class="dropdown-item nav-link text-white " href="/aplicaciones/nueva"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Subitem</a>
                             </li>
-
-
-
                         </ul>
                     </div>
 
